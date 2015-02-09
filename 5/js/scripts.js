d2a537e088ccbbe7b263bc737873ccfe9b18a37d
Steven Kutsch <skutsch40@engr-134-25.eastern.ewu.edu>
2015-02-09 14:32:21 -0800

    ok

--- 5/js/scripts.js
+++ 5/js/scripts.js
@@ -1,5 +1,20 @@
 // Check for User's Birthday
 // REF: http://codepen.io/manikoth/pen/BymXwR?editors=101
 // REF: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+var date= new Date();
+console.log(date);
 
+var birthday = {
+	month: date.getMonth(),
+	day: date.getUTCDate(),
+	check: date.getMonth() + '/' + date.getUTCDate()
+};
 
+console.log(birthday.month);
+console.log(birthday.day);
+console.log(birthday.check);
+
+if (birthday.check == '2/9'){
+		alert('HAPPY BIRTHDAY!')
+		$('.date').addClass('.bday');
+};
