fae379dd67190798f22f58d527074afca6028252
Steven Kutsch <skutsch40@engr-134-25.eastern.ewu.edu>
2015-02-09 14:16:25 -0800

    Refresher

--- 3/js/scripts.js
+++ 3/js/scripts.js
@@ -2,3 +2,13 @@
 // REF: http://api.jquery.com/contains-selector
 // REF: http://api.jquery.com/addclass/
 // Extend into an array of explitives
+
+console.log($('.text:contains(bad[2])').length);
+
+$('.text:contains("fucking")').addClass('bad');
+
+var bad=['fucking', 'dude', 'research'];
+for (var i=0; i<bad.length; i++){
+
+	console.log(bad[i]);
+}
